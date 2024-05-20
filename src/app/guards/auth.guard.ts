c220a8710107693b434f949f5e167b9baab568d3
DiegoBurgos616 <A20216388@ALUMNOS.UADY.MX>
2024-05-19 22:24:59 -0600

    Correccion de error rapido

--- src/app/guards/auth.guard.ts
+++ src/app/guards/auth.guard.ts
@@ -3,6 +3,13 @@ import { CanActivateFn, Router } from '@angular/router';
 import { JwtHelperService } from '@auth0/angular-jwt';
 
 export const authGuard: CanActivateFn = () => {
+  const jwt = inject(JwtHelperService);
+  const router = inject(Router);
+  const token = localStorage.getItem('token');
 
+  if (jwt.isTokenExpired(token)) {
+    router.navigate(['/login']);
+    return false;
+  }
   return true;
 };
